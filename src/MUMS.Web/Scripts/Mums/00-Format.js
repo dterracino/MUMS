6bb46415bacd50e8f02bcb6b5f3b3b6c2576f370
Christoffer Skeppstedt <c.skeppstedt@gmail.com>
2011-07-17 22:31:59 +0200

    Less insane prettification of download rates and file sizes.

--- src/MUMS.Web/Scripts/Mums/00-Format.js
+++ src/MUMS.Web/Scripts/Mums/00-Format.js
@@ -10,20 +10,6 @@ if (window.Mums == undefined)
 if (window.Mums.Format == undefined)
     window.Mums.Format = {};
 
-Mums.Format.PrefixMap = {
-    'gb': { range: 1073741824, precision: 1 },
-    'mb': { range: 1048576, precision: 0 },
-    'kb': { range: 1024, precision: 0 },
-    'b' : { range: 0, precision: 0 }
-};
-
-Mums.Format.TimeMap = {
-    'd' : 86400,
-    't' : 3600,
-    'm' : 60,
-    's' : 1
-};
-
 Mums.Format.Percentage = function (percentage) {
     return Math.floor(percentage);
 }
@@ -35,64 +21,35 @@ Mums.Format.Label = function (lbl) {
 }
 
 Mums.Format.PrettySize = function (bytes) {
-    if (!bytes || bytes == undefined || bytes <= 0 || bytes == NaN)
-        return "0b";
+    return Mums.Format.FriendlyRate(bytes, ["b", "kb", "mb", "gb"]);
+}
 
-    var found = false;
-    var format = "";
+Mums.Format.FriendlyRate = function GetFriendlyRate(rate, units, factor) {
 
-    $.each(Mums.Format.PrefixMap, function (prefix, setting) {
-        if (format != "")
-            return false;
+    units = units || ["b", "kb", "Mb", "Gb"];
+    factor = factor || 1024;
 
-        if (bytes >= setting.range) {
-            var number = bytes;
-            if (setting.range > 0)
-                number = bytes / setting.range;
+    for (var i = 0; i < units.length; i++) {
+        var u = units[i];
 
-            if (setting.precision > 0)
-                format = number.toFixed(setting.precision) + prefix;
+        if (rate < 10) {
+            if (i == 0)
+                return Math.round(rate) + u;
             else
-                format = Math.floor(number) + prefix;
+                return Math.round(rate * 10) / 10 + u;
         }
-    });
+        else if (rate < factor) {
+            return Math.round(rate) + u;
+        }
 
-    return format;
+        rate = rate / factor;
+    }
+
+    return "0" + units[0];
 }
 
 Mums.Format.PrettySpeed = function (bytes) {
-    if (!bytes || bytes == undefined || bytes <= 0 || bytes == NaN)
-        return "0b/s";
-
-    var found = false;
-    var format = "";
-    var prevPrefix = "";
-
-    $.each(Mums.Format.PrefixMap, function (prefix, setting) {
-        if (format != "")
-            return false;
-        
-        var range = setting.range;
-
-        if (bytes >= range) {
-            var number = bytes;
-            var div = range;
-
-            if (prevPrefix == "")
-                prevPrefix = prefix;
-            else
-                div = (range == 0) ? 1024 : range * 1024;
-
-            if (div > 0)
-                number = bytes / div;
-
-            format = number.toFixed(1) + prevPrefix + "/s";
-        }
-
-        prevPrefix = prefix;
-    });
-
-    return format;
+    return Mums.Format.FriendlyRate(bytes, ["b/s", "kb/s", "mb/s", "gb/s"]);
 }
 
 Mums.Format.PrettyEta = function (eta) {
@@ -102,7 +59,14 @@ Mums.Format.PrettyEta = function (eta) {
     var found = false;
     var format = "";
 
-    $.each(Mums.Format.TimeMap, function (suffix, range) {
+    var timeMap = {
+        'd': 86400,
+        't': 3600,
+        'm': 60,
+        's': 1
+    };
+
+    $.each(timeMap, function (suffix, range) {
         if (format != "")
             return false;
 
